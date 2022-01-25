05d2cca93e3fde0bc89a73390665c8413305575c
ktk04 <kevintkoshy4@gmail.com>
2022-01-25 16:48:24 +0400

    Restaurant Changes

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -10,23 +10,23 @@ export default function Restaurant(){
             </div>
             </div>
             <div className="row pt-4">
-             <div className="col-12 col-md-4 text-right">
+             <div className="col-12 col-md-4 text-lg-right text-center">
                <div className="search_field">
                <input type="text" className="input" placeholder="Search Restaurants" />
                <i className="fas fa-search"></i>
                </div>
              </div>
-             <div className="col-12 col-md-4 text-right">
-             <div class="dropdown">
-               <div class="default_option">All</div>  
-                <ul>
-                  <li>All</li>
-                  <li>Recent</li>
-                  <li>Popular</li>
-                </ul>
-               </div>
-             </div>
+             <div className="col-12 col-md-4 text-center">
+             <select name="Distance" id="dist">
+                <option value="1KM">1 km</option>
+                <option value="5KM">5 km</option>
+                <option value="10KM">10 km</option>
+             </select>
+            </div>
+            <div className="col-12 col-md-4 text-lg-left text-center">
+            <button className="btn btn-outline-Restaurant btn-lg mb-2"> Search </button>
             </div>
         </div>
+      </div>
     )
 }
