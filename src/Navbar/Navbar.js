a188ae735a57758384416fc2c9226fb76cab9174
ktk04 <kevintkoshy4@gmail.com>
2022-01-30 16:36:54 +0400

    Responsive Changes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import "jquery/dist/jquery.min";
 import { NavLink } from "react-router-dom";
-const Nav = () => {
+import $ from 'jquery';
+
+export default class Nav extends Component{
+    // jQuerycode=() => {
+    //     $(function(){
+    //         $('#navbarMenu ul li a').on("click",function(){
+    //             $('#navbarMenu ul').find('li.active').removeClass('active');
+    //             $(this).parent().addClass('active');
+        
+    //             return false; 
+    //         });
+    //     });
+    //       }
+
+    //       componentDidMount(){
+    //         this.jQuerycode()
+    //       }
+
+    render(){
     return(
         <nav className="navbar navbar-expand-md navbar-dark" >
         <a href="/" className="navbar-brand logo"> Srm Hub </a>
@@ -11,21 +29,20 @@ const Nav = () => {
         <div className="collapse navbar-collapse" id="navbarMenu">
         <ul className="navbar-nav ml-auto">
             <li className="nav-item px-3">
-                <a href="/" className="nav-link"> Home</a>
+                <NavLink end="active-link" to="/" className="nav-link"> Home</NavLink>
             </li>
             <li className="nav-item px-3">
-                <a href="/notes" className="nav-link"> Notes </a>
+                <NavLink end="active-link" to="/notes" className="nav-link"> Notes </NavLink>
             </li>
             <li className="nav-item px-3">
-                <a href="/restaurants" className="nav-link"> Restaurants </a>
+                <NavLink end="active-link" to="/restaurants" className="nav-link"> Restaurants </NavLink>
             </li>
             <li className="nav-item px-3">
-                <a href="/contact" className="nav-link"> Contact Us </a>
+                <NavLink end="active-link" to="/contact" className="nav-link"> Contact Us </NavLink>
             </li>
         </ul>
         </div>
         </nav>
-    )
+      )
+     } 
     }
-
-    export default Nav;
